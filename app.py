5f643f9e803e8c3005b71fd3f5eb55801ed15a7e
minhthang1233 <nkoccoi321@gmail.com>
2024-10-09 18:56:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -13,17 +13,21 @@ pages = {}
 def generate_random_id(length=6):
     return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
 
-# Hàm để thay thế các liên kết bằng thẻ HTML anchor
+# Hàm để thay thế các liên kết và ký tự xuống dòng bằng thẻ HTML
 def convert_links(text):
+    # Thay thế link thành thẻ <a>
     url_pattern = r"(https?://[^\s]+)"
-    return re.sub(url_pattern, r'<a href="\1">\1</a>', text)
+    text_with_links = re.sub(url_pattern, r'<a href="\1">\1</a>', text)
+    
+    # Thay thế ký tự xuống dòng (\n) bằng thẻ <br>
+    return text_with_links.replace('\n', '<br>')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         content = request.form['content']
         random_id = generate_random_id()
-        # Chuyển đổi các link thành thẻ a
+        # Chuyển đổi các link và xuống dòng
         content_with_links = convert_links(content)
         pages[random_id] = content_with_links
         return redirect(url_for('view_page', page_id=random_id))
